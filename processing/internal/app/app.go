09e8b44714fea5e936012f1a280a7fbfb7e51dd0
um4ru-ch4n <17515560@mail.ru>
2021-12-01 17:00:36 +0300

    get metrics with prometheus testing

--- processing/internal/app/app.go
+++ processing/internal/app/app.go
@@ -2,15 +2,19 @@ package app
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/defaulterrr/elegant_swirles/processing/internal/config"
 	"github.com/defaulterrr/elegant_swirles/processing/internal/model"
 	"github.com/defaulterrr/elegant_swirles/processing/internal/repository"
+	"github.com/defaulterrr/elegant_swirles/processing/internal/server"
 	"github.com/defaulterrr/elegant_swirles/processing/internal/service"
+	"github.com/defaulterrr/elegant_swirles/processing/metrics"
 )
 
 func Run() {
@@ -25,17 +29,38 @@ func Run() {
 		os.Exit(1)
 	}
 
+	err = metrics.InitMetrics()
+	if err != nil {
+		fmt.Printf("metrics.InitMetrics: %v", err)
+		os.Exit(1)
+	}
+
 	conns, err := repository.GetGRPCConns(&config.Grpc)
 	if err != nil {
 		log.Fatalf("repository.GetGRPCConns: %v", err)
 	}
 
-	defer conns.DHTConn.Close()
+	defer func() {
+		err = conns.DHTConn.Close()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
 
 	repo := repository.NewRepository(conns)
 	serv := service.NewService(repo)
 
 	// testing GetDHTMetrics function
+	metricsServer := server.CreateMetricsServer(&config.Metrics)
+
+	go func() {
+		fmt.Printf("Metrics server is running on %s:%d\n", config.Metrics.Host, config.Metrics.Port)
+		if err = metricsServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("Failed running metrics server:%v\n", err)
+			os.Exit(1)
+		}
+	}()
+
 	curMetrics := make(chan model.DHTMetrics)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
@@ -43,12 +68,17 @@ func Run() {
 	go func() {
 		err = serv.GetDHTMetrics(ctx, curMetrics)
 		if err != nil {
-			fmt.Printf("GetDHTMetrics: %v", err)
+			fmt.Printf("GetDHTMetrics: %v\n", err)
 		}
 	}()
 
 	for i := 0; i < 200; i++ {
-		metr := <-curMetrics
+		metr, ok := <-curMetrics
+		if !ok {
+			break
+		}
+
+		metrics.SetTemperature(float64(metr.Temperature))
 		fmt.Println(metr)
 	}
 
